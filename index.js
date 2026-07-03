e57450f9051d93f37678dce112519081a8efdf6f
wwg98 <wwwg98@gmail.com>
2026-07-03 10:15:39 +0900

    Fix formatting of middleware usage in Express server

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const app = express();
 const mysql = require("mysql2");
 const port = 3000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true })); //json->object
+app.use(express.json()); //json->object
+app.use(express.urlencoded({ extended: true })); //html form ->object
 
 const corsOptions = {
   origin: "*",
