03667c543c384c5414cec6308ecbee0ed3e86c3b
wwg98 <wwwg98@gmail.com>
2026-07-06 11:50:19 +0900

    Implement code changes to enhance functionality and improve performance

--- index.js
+++ index.js
@@ -3,15 +3,29 @@ const cors = require("cors");
 const app = express();
 const mysql = require("mysql2");
 const port = 3000;
+const multer = require("multer");
 
 app.use(express.json()); //json->object
 app.use(express.urlencoded({ extended: true })); //html form ->object
 
-const corsOptions = {
+let corsOptions = {
   origin: "*",
 };
 
 app.use(cors(corsOptions));
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    const orinalExt = file.originalname.split(".")[1];
+    const uniquePrefix = Date.now() + "-" + Math.round(Math.random() * 1000);
+    cb(null, uniquePrefix + "-" + file.fieldname + "." + orinalExt);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 const db = mysql.createConnection({
   host: "localhost",
@@ -19,6 +33,7 @@ const db = mysql.createConnection({
   password: "wlq23128371@",
   database: "bbs",
 });
+
 db.connect();
 
 app.get("/", (req, res) => {
@@ -35,7 +50,9 @@ app.get("/list", (req, res) => {
 });
 
 app.get("/view", (req, res) => {
+  console.log(req.query.id);
   const id = req.query.id;
+  // const sqlQuery = `SELECT * FROM board WHERE id=${req.query.id};`;
   const sqlQuery =
     "SELECT title, content, writer, DATE_FORMAT(date, '%Y-%m-%d') AS date FROM board WHERE id=?;";
   db.query(sqlQuery, [id], (err, result) => {
@@ -44,30 +61,23 @@ app.get("/view", (req, res) => {
   });
 });
 
-app.post("/write", (req, res) => {
-  const { title, name, content } = req.body;
-  const sqlQuery = "INSERT INTO board (title, content, writer) VALUES (?, ?, ?)";
+app.post("/write", upload.single("image"), (req, res) => {
+  console.log(req.body);
+  const { title, writer, content } = req.body;
+  const imagePath = req.file ? req.file.path : null; //req.file.path는 업로드된 파일의 경로
 
-  db.query(sqlQuery, [title, content, name], (err, result) => {
-    if (err) throw err;
-    res.send(result);
-  });
-});
-
-app.post("/update", (req, res) => {
-  const { name, title, content, id } = req.body;
-  const sqlQuery = "UPDATE board SET writer=?, title=?, content=? WHERE id=?;";
-
-  db.query(sqlQuery, [name, title, content, id], (err, result) => {
+  const sqlQuery = "insert into board (title,content,writer,image_path) values (?,?,?,?);";
+  db.query(sqlQuery, [title, content, writer, imagePath], (err, result) => {
     if (err) throw err;
     res.send(result);
   });
 });
 
 app.post("/delete", (req, res) => {
+  console.log(req.body);
   const { id } = req.body;
-  const sqlQuery = "DELETE FROM board WHERE id=?;";
 
+  const sqlQuery = "DELETE FROM board WHERE id=?";
   db.query(sqlQuery, [id], (err, result) => {
     if (err) throw err;
     res.send(result);
@@ -75,10 +85,22 @@ app.post("/delete", (req, res) => {
 });
 
 app.post("/deleteselect", (req, res) => {
+  console.log(req.body);
   const { boardIdList } = req.body;
-  const sqlQuery = `DELETE FROM board WHERE id in (${boardIdList});`;
 
+  const sqlQuery = `delete from board where id in (${boardIdList})`;
   db.query(sqlQuery, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+app.post("/update", (req, res) => {
+  console.log(req.body);
+  const { name, title, content, id } = req.body;
+
+  const sqlQuery = "UPDATE board SET writer=?, title=?, content=? WHERE id=?";
+  db.query(sqlQuery, [name, title, content, id], (err, result) => {
     if (err) throw err;
     res.send(result);
   });
