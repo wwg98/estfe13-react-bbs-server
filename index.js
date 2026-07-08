60023b83c60c906f8193bfd590344967a234ebf6
wwg98 <wwwg98@gmail.com>
2026-07-08 13:12:07 +0900

    Update database user and adjust image path handling in post requests

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const upload = multer({ storage: storage });
 
 const db = mysql.createConnection({
   host: "localhost",
-  user: "root",
+  user: "user_bbs",
   password: "wlq23128371@",
   database: "bbs",
 });
@@ -76,7 +76,7 @@ app.get("/view", (req, res) => {
 app.post("/write", upload.single("image"), (req, res) => {
   console.log(req.body);
   const { title, writer, content } = req.body;
-  const imagePath = req.file ? req.file.path : null; //req.file.path는 업로드된 파일의 경로
+  const imagePath = req.file ? `/uploads/${req.file.filename}` : null; //req.file.path는 업로드된 파일의 경로
 
   const sqlQuery = "insert into board (title,content,writer,image_path) values (?,?,?,?);";
   db.query(sqlQuery, [title, content, writer, imagePath], (err, result) => {
@@ -128,7 +128,7 @@ app.post("/deleteselect", (req, res) => {
 app.post("/update", upload.single("image"), (req, res) => {
   console.log(req.body);
   const { writer, title, content, id, remove_image } = req.body;
-  const imagePath = req.file ? req.file.path : null; //새이미지 정보 할당
+  const imagePath = req.file ? `/uploads/${req.file.filename}` : null; //새이미지 정보 할당
   const shouldRemoveImage = remove_image === "1";
   let sqlQuery;
   let params;
